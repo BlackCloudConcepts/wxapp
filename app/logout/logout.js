f623ea794a4f2491bac3e321b7b841360cbf247f
blackcloudconcepts <blackcloudconcepts@gmail.com>
2014-11-29 14:45:54 -0500

    added unauth for firebase on client logout

--- app/logout/logout.js
+++ app/logout/logout.js
@@ -18,6 +18,8 @@ LogoutController.$inject =  ['$scope'];
 function LogoutController($scope) {
 	var self = this;
 
+	var firebaseObj = new Firebase("https://resplendent-heat-1209.firebaseio.com/wx/");
+	firebaseObj.unauth();
 	document.cookie = 'username=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 	
 	location = '#login';
