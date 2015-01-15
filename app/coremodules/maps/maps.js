0e653c8e18fddec2e28bcba993985e9ce5604dc1
blackcloudconcepts <blackcloudconcepts@gmail.com>
2015-01-14 21:14:36 -0500

    adding geoJSON data to maps, experimenting with Open Layers

--- app/coremodules/maps/maps.js
+++ app/coremodules/maps/maps.js
@@ -7,11 +7,69 @@ MapsService.$inject = ['ConversionsService'];
 
 function MapsService(ConversionsService){
 
+	// generic mapping function to route to appropriate service
+	// input: arrCities - array of cities and their related data
+        //      : currentRegion - current region of cities to limit list to and focus map on
+        //      : containerId - div to put the map in   
+	this.drawMap = function(arrCities, currentRegion, containerId){
+		this.drawGoogleMap(arrCities, currentRegion, containerId);
+	}
+
+
+	// draws open layer map 
+	// input: arrCities - array of cities and their related data
+        //      : currentRegion - current region of cities to limit list to and focus map on
+        //      : containerId - div to put the map in   
+	this.drawOpenLayerMap = function(arrCities, currentRegion, containerId){
+		// filter out cities by region
+                var arr = [];
+                for (var i = 0;i < arrCities.length; i++){
+                        if (arrCities[i].region == currentRegion){
+                                arr.push(arrCities[i]);
+                        }
+                }
+                arrCities = arr;
+
+		// http://www.peterrobins.co.uk/it/olchangingprojection.html
+		//Maps Center 
+		var lat = arrCities[0].coord.lat; 
+		var lon = arrCities[0].coord.lon;
+console.log(lat);
+console.log(lon);
+//		lat = 7486473;
+//		lon = 4193332;
+		var lonlat = new OpenLayers.LonLat(lon, lat);
+	    	var map = new OpenLayers.Map(containerId);
+
+
+		// Create overlays
+
+		// OSM layer
+	    	var mapnik = new OpenLayers.Layer.OSM();
+		// stations layer
+		var stations = new OpenLayers.Layer.Vector.OWMStations("Stations");
+		// weather layer
+		var city = new OpenLayers.Layer.Vector.OWMWeather("Weather");
+
+		//connect layers to map
+		map.addLayers([mapnik, stations, city]);
+
+		//Add Layers swither
+		map.addControl(new OpenLayers.Control.LayerSwitcher());       	
+
+		map.setCenter( lonlat, 10 );
+
+		// Add popups 
+		var selectControl = new OpenLayers.Control.SelectFeature([city,stations]);
+		map.addControl(selectControl);
+		selectControl.activate(); 
+	}
+
 	// draws google map with markers and popups for indicated locations
 	// input: arrCities - array of cities and their related data
 	//      : currentRegion - current region of cities to limit list to and focus map on
 	//      : containerId - div to put the map in	
-	this.drawMap = function(arrCities, currentRegion, containerId){
+	this.drawGoogleMap = function(arrCities, currentRegion, containerId){
 		// filter out cities by region
 		var arr = [];
 		for (var i = 0;i < arrCities.length; i++){
@@ -27,6 +85,48 @@ function MapsService(ConversionsService){
 			zoom: 8
 		};
 		var map = new google.maps.Map(document.getElementById(containerId), mapOptions);
+	
+		// build any visualization data dynamically
+		// http://geojson.org/geojson-spec.html#linestring	
+		var myData = {
+		  "type": "FeatureCollection",
+		  "features": [
+		    {
+		      "type": "Feature",
+		      "properties": {
+			"letter": "G",
+			"color": "blue",
+			"rank": "7",
+			"ascii": "71"
+		      },
+		      "geometry": {
+			"type": "Polygon",
+//			"type": "MultiLineString",
+			"coordinates": [
+			  [
+			    [123.61, -22.14], [122.38, -21.73], [121.06, -21.69], [119.66, -22.22], [119.00, -23.40],
+			    [118.65, -24.76], [118.43, -26.07], [118.78, -27.56], [119.22, -28.57], [120.23, -29.49],
+			    [121.77, -29.87], [123.57, -29.64], [124.45, -29.03], [124.71, -27.95], [124.80, -26.70],
+			    [124.80, -25.60], [123.61, -25.64], [122.56, -25.64], [121.72, -25.72], [121.81, -26.62],
+			    [121.86, -26.98], [122.60, -26.90], [123.57, -27.05], [123.57, -27.68], [123.35, -28.18],
+			    [122.51, -28.38], [121.77, -28.26], [121.02, -27.91], [120.49, -27.21], [120.14, -26.50],
+			    [120.10, -25.64], [120.27, -24.52], [120.67, -23.68], [121.72, -23.32], [122.43, -23.48],
+			    [123.04, -24.04], [124.54, -24.28], [124.58, -23.20], [123.61, -22.14]
+			  ]
+			]
+		      }
+		    }
+		  ]
+		};
+
+		map.data.addGeoJson(myData);
+//		map.data.loadGeoJson('https://storage.googleapis.com/maps-devrel/google.json');
+
+		var featureStyle = {
+		    fillColor: 'green',
+		    strokeWeight: 1
+		}
+		map.data.setStyle(featureStyle);
 
 		// add markers
 		var markers = {};
--- app/prebuild-traceur/coremodules/maps/maps.js
+++ app/prebuild-traceur/coremodules/maps/maps.js
@@ -2571,6 +2571,33 @@ $traceurRuntime.ModuleStore.getAnonymousModule(function() {
     MapsService.$inject = ['ConversionsService'];
     function MapsService(ConversionsService) {
       this.drawMap = function(arrCities, currentRegion, containerId) {
+        this.drawGoogleMap(arrCities, currentRegion, containerId);
+      };
+      this.drawOpenLayerMap = function(arrCities, currentRegion, containerId) {
+        var arr = [];
+        for (var i = 0; i < arrCities.length; i++) {
+          if (arrCities[i].region == currentRegion) {
+            arr.push(arrCities[i]);
+          }
+        }
+        arrCities = arr;
+        var lat = arrCities[0].coord.lat;
+        var lon = arrCities[0].coord.lon;
+        console.log(lat);
+        console.log(lon);
+        var lonlat = new OpenLayers.LonLat(lon, lat);
+        var map = new OpenLayers.Map(containerId);
+        var mapnik = new OpenLayers.Layer.OSM();
+        var stations = new OpenLayers.Layer.Vector.OWMStations("Stations");
+        var city = new OpenLayers.Layer.Vector.OWMWeather("Weather");
+        map.addLayers([mapnik, stations, city]);
+        map.addControl(new OpenLayers.Control.LayerSwitcher());
+        map.setCenter(lonlat, 10);
+        var selectControl = new OpenLayers.Control.SelectFeature([city, stations]);
+        map.addControl(selectControl);
+        selectControl.activate();
+      };
+      this.drawGoogleMap = function(arrCities, currentRegion, containerId) {
         var arr = [];
         for (var i = 0; i < arrCities.length; i++) {
           if (arrCities[i].region == currentRegion) {
@@ -2586,6 +2613,28 @@ $traceurRuntime.ModuleStore.getAnonymousModule(function() {
           zoom: 8
         };
         var map = new google.maps.Map(document.getElementById(containerId), mapOptions);
+        var myData = {
+          "type": "FeatureCollection",
+          "features": [{
+            "type": "Feature",
+            "properties": {
+              "letter": "G",
+              "color": "blue",
+              "rank": "7",
+              "ascii": "71"
+            },
+            "geometry": {
+              "type": "Polygon",
+              "coordinates": [[[123.61, -22.14], [122.38, -21.73], [121.06, -21.69], [119.66, -22.22], [119.00, -23.40], [118.65, -24.76], [118.43, -26.07], [118.78, -27.56], [119.22, -28.57], [120.23, -29.49], [121.77, -29.87], [123.57, -29.64], [124.45, -29.03], [124.71, -27.95], [124.80, -26.70], [124.80, -25.60], [123.61, -25.64], [122.56, -25.64], [121.72, -25.72], [121.81, -26.62], [121.86, -26.98], [122.60, -26.90], [123.57, -27.05], [123.57, -27.68], [123.35, -28.18], [122.51, -28.38], [121.77, -28.26], [121.02, -27.91], [120.49, -27.21], [120.14, -26.50], [120.10, -25.64], [120.27, -24.52], [120.67, -23.68], [121.72, -23.32], [122.43, -23.48], [123.04, -24.04], [124.54, -24.28], [124.58, -23.20], [123.61, -22.14]]]
+            }
+          }]
+        };
+        map.data.addGeoJson(myData);
+        var featureStyle = {
+          fillColor: 'green',
+          strokeWeight: 1
+        };
+        map.data.setStyle(featureStyle);
         var markers = {};
         for (var i = 0; i < arrCities.length; i++) {
           (function() {
